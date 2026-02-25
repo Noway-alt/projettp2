4d7ec96a2fad141503cfcb22d9ce29d35343c2d3
Noway-alt <lannoonoe8@gmail.com>
2026-02-25 17:57:20 +0100

    Update AnswerType

--- src/Form/AnswerType.php
+++ src/Form/AnswerType.php
@@ -6,6 +6,8 @@ use App\Entity\Answer;
 use App\Entity\Question;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,11 +16,25 @@ class AnswerType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('text')
-            ->add('isCorrect')
+            ->add('text', TextType::class, [
+                'label' => 'Texte de la réponse',
+                'attr' => [
+                    'placeholder' => 'Entrez la réponse...',
+                    'class' => 'form-control'
+                ]
+            ])
+            ->add('isCorrect', CheckboxType::class, [
+                'label' => 'Cette réponse est correcte ?',
+                'required' => false,
+            ])
             ->add('question', EntityType::class, [
                 'class' => Question::class,
-                'choice_label' => 'id',
+                'choice_label' => 'content',
+                'label' => 'Question associée',
+                'placeholder' => 'Choisir une question',
+                'attr' => [
+                    'class' => 'form-control'
+                ]
             ])
         ;
     }
